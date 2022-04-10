a2a17202a9f70df3ca47c48216e1f3324e0f317f
Sebastien <gith.sebastien@gmail.com>
2022-04-10 23:22:24 +0200

    upload the project

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,17 @@
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+  <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none ms-2">
+    {{ __('Newspaper') }}
+  </a>
+
+  <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+    <li><a href="#" class="nav-link px-2 link-secondary">{{ __('Accueil') }}</a></li>
+    <li><a href="#" class="nav-link px-2 link-dark">{{ __('Articles') }}</a></li>
+    <li><a href="#" class="nav-link px-2 link-dark">{{ __('Creer un article') }}</a></li>
+    <li><a href="#" class="nav-link px-2 link-dark">{{ __('A propos') }}</a></li>
+  </ul>
+
+  <div class="col-md-3 text-end">
+    <button type="button" class="btn me-2 text-dark">{{ __('Se connecter') }}</button>
+    <button type="button" class="btn text-dark">{{ __('Créer un compte') }}</button>
+  </div>
+</header>
