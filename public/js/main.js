76d8902fe41ef4713a0bba2f547489ac3c901aed
Sebastien <gith.sebastien@gmail.com>
2022-04-24 02:24:11 +0200

    using class for style background when srolling

--- public/js/main.js
+++ public/js/main.js
@@ -7,23 +7,28 @@ if (button) {
     })
 };
 
-let anchorAppear = function() {
-    let position = 177;
-    let header = document.querySelector('header');
-    let html = document.querySelector('html');
-    let x = html.scrollTop;
-    if (x > position) {
-        header.style.backgroundColor = "rgba(0,0,0,.6)";
-        header.style.transition = ".2s ease-in-out";
-        header.classList.add('text');
+class HeaderBackground {
+    set_Header_Background_Property() {
+        let position = 177;
+        let header = document.querySelector('header');
+        let html = document.querySelector('html');
+        let htmlPosition = html.scrollTop;
+        if (htmlPosition > position) {
+            header.style.backgroundColor = "rgba(0,0,0,.6)";
+            header.style.transition = ".2s ease-in-out";
+            header.classList.add('text');
 
-    } else {
-        header.style.backgroundColor = "white";
-        header.classList.remove('text');
+        } else {
+            header.style.backgroundColor = "white";
+            header.classList.remove('text');
+        }
     }
 }
 
 
+let headerBackground = new HeaderBackground();
+
+
 window.addEventListener('scroll', () => {
-    anchorAppear();
-});
+    headerBackground.set_Header_Background_Property();
+})
